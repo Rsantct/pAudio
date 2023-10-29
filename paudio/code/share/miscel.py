df069bf9ca50f71bcadecd659082fe6573fec2ed
Rsantct <rsantct@gmail.com>
2023-10-29 23:41:16 +0100

    Update and rename misc.py to miscel.py

---
+++ paudio/code/share/miscel.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Rafael Sánchez
+
+
+def read_cmd_phrase(cmd_phrase):
+    """
+        Command phrase SYNTAX must start with an appropriate prefix:
+
+            preamp  command  arg1 ... [add]
+            players command  arg1 ...
+            aux     command  arg1 ...
+
+        The 'preamp' prefix can be omited
+
+        The 'add' option for relative level, bass, treble, ...
+    """
+
+    pfx, cmd, argstring, add = '', '', '', False
+
+    # This is to avoid empty values when there are more
+    # than on space as delimiter inside the cmd_phrase:
+    chunks = [x for x in cmd_phrase.split(' ') if x]
+
+    if 'add' in chunks:
+        add = True
+        chunks.remove('add')
+
+    # If not prefix, will treat as a preamp command kind of
+    if not chunks[0] in ('preamp', 'player', 'aux'):
+        chunks.insert(0, 'preamp')
+    pfx = chunks[0]
+
+    if chunks[1:]:
+        cmd = chunks[1]
+    if chunks[2:]:
+        # <argstring> can be compound
+        argstring = ' '.join( chunks[2:] )
+
+    return pfx, cmd, argstring, add
+
+
+def x2int(x):
+    return int(round(float(x)))
+
+
+def x2float(x):
+    return round(float(x),1)
+
+def x2bool(x, curr=False):
+    if x.lower() == 'toggle':
+        return {True:False, False:True}[curr]
+    elif x.lower() in ['true', 'on', '1']:
+        return True
+    else:
+        return False
+
