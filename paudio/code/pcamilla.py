4c249db27f9c38f7f0f5dca0ec03c00ba24245a3
Rsantct <rsantct@gmail.com>
2023-11-02 00:19:30 +0100

    Update pcamilla.py

--- paudio/code/pcamilla.py
+++ paudio/code/pcamilla.py
@@ -4,6 +4,7 @@
 
 import  subprocess as sp
 from    time import sleep
+import  yaml
 import  json
 from    camilladsp import CamillaConnection
 import  make_eq as mkeq
@@ -20,25 +21,97 @@ eq_link = '../eq/eq.pcm'
 sp.Popen(f'cp {eq_flat_path} {eq_A_path}'.split())
 sp.Popen(f'cp {eq_flat_path} {eq_B_path}'.split())
 
-
-# Starting CamillaDSP (muted)
-sp.call("pkill camilladsp".split())
-sp.Popen("camilladsp -m -a 127.0.0.1 -p 1234 camilladsp.yml".split())
-sleep(1)
-PC = CamillaConnection("127.0.0.1", 1234)
-
-PC.connect()
-
-# Initial config snapshot
-CFG_INIT = PC.get_config()
+PC = None
+CFG_INIT = None
 
 
 # INTERNAL
 
-def set_device(devID):
-    cfg = PC.get_config()
-    cfg["devices"]["playback"]["device"] = devID
-    set_config_sync(cfg)
+def clear_pipeline_drc(cfg):
+    for n in (1,2):
+        names_old = cfg["pipeline"][n]['names']
+        names_new = list_remove_by_pattern(names_old, 'drc.')
+        cfg["pipeline"][n]['names'] = names_new
+    return cfg
+
+
+def make_pipeline_drc(cfg, drcID):
+    for n in (1,2):
+        names_old = cfg["pipeline"][n]['names']
+        names_new = list_remove_by_pattern(names_old, 'drc.')
+        names_new.insert(1, f'drc.L.{drcID}')
+        cfg["pipeline"][n]['names'] = names_new
+    return cfg
+
+
+def init_camilladsp(user_config, drc_sets=[]):
+    """ Updates camilladsp.yml with user configs,
+        then runs camilladsp process
+    """
+
+    def update_config():
+        """ Updates camilladsp.yml with user configs
+        """
+
+        def update_drc_stuff(cfg):
+
+            def clear_drc_filters(cfg):
+                keys = []
+                for f in cfg["filters"]:
+                    if f.startswith('drc.'):
+                        keys.append(f)
+                for k in keys:
+                    del cfg["filters"][k]
+                return cfg
+
+            def make_drc_filters(cfg):
+                lspk = user_config["loudspeaker"]
+                for ID in drc_sets:
+                    for Ch in 'L', 'R':
+                        cfg["filters"][f'drc.{Ch}.{ID}'] = {}
+                        cfg["filters"][f'drc.{Ch}.{ID}']["type"] = 'Conv'
+                        cfg["filters"][f'drc.{Ch}.{ID}']["parameters"] = {}
+                        cfg["filters"][f'drc.{Ch}.{ID}']["parameters"]["filename"] = f'../loudspeakers/{lspk}/drc.L.{ID}.pcm'
+                        cfg["filters"][f'drc.{Ch}.{ID}']["parameters"]["format"] = 'FLOAT32LE'
+                        cfg["filters"][f'drc.{Ch}.{ID}']["parameters"]["type"] = 'Raw'
+                return cfg
+
+            clear_drc_filters(cfg)
+            make_drc_filters(cfg)
+            clear_pipeline_drc(cfg)
+            make_pipeline_drc(cfg, drc_sets[0])
+
+            return cfg
+
+        with open('camilladsp.yml', 'r') as f:
+            camilla_cfg = yaml.safe_load(f)
+
+        # Audio Device
+        camilla_cfg["devices"]["playback"]["device"] = user_config["device"]
+
+        # DRCs
+        if drc_sets:
+            camilla_cfg = update_drc_stuff(camilla_cfg)
+
+        with open('camilladsp.yml', 'w') as f:
+            yaml.safe_dump(camilla_cfg, f)
+
+
+    global PC, CFG_INIT
+
+    # Updating user configs ---> camilladsp.yml
+    update_config()
+
+    # Starting CamillaDSP with <camilladsp.yml> <muted>
+    sp.call("pkill camilladsp".split())
+    sp.Popen("camilladsp -m -a 127.0.0.1 -p 1234 camilladsp.yml".split())
+    sleep(1)
+    PC = CamillaConnection("127.0.0.1", 1234)
+    PC.connect()
+
+    # Initial config snapshot
+    CFG_INIT = PC.get_config()
+
 
 def set_config_sync(cfg):
     PC.set_config(cfg)
@@ -58,12 +131,12 @@ def get_pipeline():
     return json.dumps( PC.get_config()["pipeline"] )
 
 
-def toggle_last_eq():
-    global last_eq
-    last_eq = {'A':'B', 'B':'A'}[last_eq]
-
-
 def reload_eq():
+
+    def toggle_last_eq():
+        global last_eq
+        last_eq = {'A':'B', 'B':'A'}[last_eq]
+
 
     mkeq.make_eq()
     eq_path     = f'../eq/eq_{last_eq}.pcm'
@@ -87,7 +160,7 @@ def get_drc_sets():
     filters = CFG_INIT["filters"]
     drc_sets = []
     for f in filters:
-        if f.startswith('drc'):
+        if f.startswith('drc.'):
             drc_set = f.split('.')[-1]
             if not drc_set in drc_sets:
                 drc_sets.append(drc_set)
@@ -153,10 +226,7 @@ def set_drc(drcID):
 
     if drcID == 'none':
         try:
-            for n in (1,2):
-                old = cfg["pipeline"][n]['names']
-                new = list_remove_by_pattern(old, 'drc.')
-                cfg["pipeline"][n]['names'] = new
+            cfg = clear_pipeline_drc(cfg)
             set_config_sync(cfg)
             result = 'done'
         except Exception as e:
@@ -164,8 +234,7 @@ def set_drc(drcID):
 
     else:
         try:
-            cfg["pipeline"][1]['names'] = CFG_INIT["pipeline"][1]['names']
-            cfg["pipeline"][2]['names'] = CFG_INIT["pipeline"][2]['names']
+            cfg = make_pipeline_drc(cfg, drcID)
             set_config_sync(cfg)
             result = 'done'
         except Exception as e:
@@ -177,6 +246,13 @@ def set_drc(drcID):
 def set_drc_gain(dB):
     cfg = PC.get_config()
     cfg["filters"]["drc_gain"]["parameters"]["gain"] = dB
+    set_config_sync(cfg)
+    return 'done'
+
+
+def set_lu_offset(dB):
+    cfg = PC.get_config()
+    cfg["filters"]["lu_offset"]["parameters"]["gain"] = dB
     set_config_sync(cfg)
     return 'done'
 
