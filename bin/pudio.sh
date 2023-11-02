2444c1cfd70f7b800e20a936c8d5477b23adef83
Rsantct <rsantct@gmail.com>
2023-11-02 23:51:34 +0100

    Update and rename pAudio.sh to pudio.sh

---
+++ bin/pudio.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Verbose mode
+python3 ~/paudio/code/share/server.py paudio 0.0.0.0 9990 -v
+
+# Silent mode
+# python3 ~/paudio/code/share/server.py paudio 0.0.0.0 9990 1>/dev/null 2>&1 &
+
