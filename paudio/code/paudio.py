27af11c6754862f49b3ff03186d38645ea8508c4
Rsantct <rsantct@gmail.com>
2023-11-02 18:34:31 +0100

    Update paudio.py

--- paudio/code/paudio.py
+++ paudio/code/paudio.py
@@ -2,43 +2,18 @@
 
 # Copyright (c) Rafael Sánchez
 
-import  json
 import  preamp
 from    miscel import *
 
-state = {}
 
+def do(cmd_phrase):
 
-def read_state_from_disk():
-    global state
-    with open('../.state', 'r') as f:
-        state = json.loads(f.read())
-
-
-def save_state():
-    with open('../.state', 'w') as f:
-        f.write(json.dumps(state))
-
-
-def init():
-    read_state_from_disk()
-    # this links the 'state' variable inside preamp
-    preamp.state = state
-    preamp.resume_audio_settings()
-
-
-def do(cph):
-
-    prefix, cmd, args, add = read_cmd_phrase(cph)
+    prefix, cmd, args, add = read_cmd_phrase(cmd_phrase)
     result    = ''
-    dosave    = True
 
     match prefix:
 
         case 'preamp':
-            # Some commands does not need to save the state
-            if cmd == 'state' or cmd.startswith('get_'):
-                dosave = False
             result = preamp.do(cmd, args, add)
 
         case 'aux':
@@ -51,13 +26,9 @@ def do(cph):
             pass
 
         case _:
-            result = 'unknown'
+            # This should not occur because preamp is the default prefix
+            result = 'unknown subsystem'
 
-    if dosave:
-        save_state()
 
     return result
 
-
-# INIT
-init()
