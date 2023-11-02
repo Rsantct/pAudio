0d6356111293edc27e4fca4933ed5af0e0f1e964
Rsantct <rsantct@gmail.com>
2023-11-02 18:37:05 +0100

    Update miscel.py

--- paudio/code/share/miscel.py
+++ paudio/code/share/miscel.py
@@ -3,13 +3,26 @@
 # Copyright (c) Rafael Sánchez
 
 import  yaml
+import  json
+from    fmt import Fmt
 import  os
 UHOME = os.path.expanduser('~')
-MAINFOLDER = f'{UHOME}/paudio'
+MAINFOLDER      = f'{UHOME}/paudio'
+EQFOLDER        = f'{MAINFOLDER}/eq'
+
+def read_json_file(fpath):
+    with open(fpath, 'r') as f:
+        d = json.loads(f.read())
+    return d
 
 
-def read_user_config():
-    with open(f'{MAINFOLDER}/config.yml', 'r') as f:
+def save_json_file(d, fpath):
+    with open(fpath, 'w') as f:
+        f.write(json.dumps(d))
+
+
+def read_yaml_file(fpath):
+    with open(fpath, 'r') as f:
         c = yaml.safe_load(f.read())
     return c
 
@@ -95,15 +108,14 @@ def get_drc_sets_from_loudspeaker(lspk):
     except:
         pass
 
-    if drc_files:
-        for f in drc_files:
-            chID  = f.split('.')[1]
-            drcID = '.'.join(f.split('.')[2:]).replace('.pcm', '')
-            if not drcID in drc_sets_candidate:
-                drc_sets_candidate[drcID] = [chID]
-            else:
-                if not chID in drc_sets_candidate[drcID]:
-                    drc_sets_candidate[drcID].append(chID)
+    for f in drc_files:
+        chID  = f.split('.')[1]
+        drcID = '.'.join(f.split('.')[2:]).replace('.pcm', '')
+        if not drcID in drc_sets_candidate:
+            drc_sets_candidate[drcID] = [chID]
+        else:
+            if not chID in drc_sets_candidate[drcID]:
+                drc_sets_candidate[drcID].append(chID)
 
     for k in drc_sets_candidate:
         channels = drc_sets_candidate[k]
@@ -111,3 +123,28 @@ def get_drc_sets_from_loudspeaker(lspk):
             drc_sets.append(k)
 
     return sorted(drc_sets)
+
+
+def get_target_sets(fs=44100):
+    """ looks for '+x.x-x.x_target_mag.dat files inside the eq folder
+    """
+    targets_folder  = f'{EQFOLDER}/curves_{fs}_N11/room_target'
+    files = []
+    sets  = []
+
+    try:
+        files = os.listdir(targets_folder)
+        files = [x for x in files if os.path.isfile(f'{targets_folder}/{x}') ]
+        files = [x for x in files if x.endswith('_target_mag.dat') ]
+    except:
+        pass
+
+    for file in files:
+        tID = file.split('_target')[0]
+        if not tID in sets:
+            sets.append(tID)
+
+    return sorted(sets)
+
+
+
