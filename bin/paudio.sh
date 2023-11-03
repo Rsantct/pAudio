d90bbec03eb0a96adfe3377d0b4cdee3dd2651e3
Rsantct <rsantct@gmail.com>
2023-11-04 00:45:05 +0100

    Update paudio.sh

--- bin/paudio.sh
+++ bin/paudio.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+pkill -f "server.py paudio"
+pkill -f "camilladsp.yml"
+if [[ $1 = *"-s"* ]]; then
+    echo "stopped."
+    exit 0
+fi
+
 # Verbose mode
-python3 ~/paudio/code/share/server.py paudio 0.0.0.0 9990 -v
+if [[ $1 = *"-v"* ]]; then
+    python3 ~/paudio/code/share/server.py paudio 0.0.0.0 9990 -v
 
 # Silent mode
-# python3 ~/paudio/code/share/server.py paudio 0.0.0.0 9990 1>/dev/null 2>&1 &
-
+else
+    python3 ~/paudio/code/share/server.py paudio 0.0.0.0 9990 1>/dev/null 2>&1 &
+    echo "server running in background ..."
+fi
