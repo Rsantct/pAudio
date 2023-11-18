d656487c802bedc0def0ce1655d3a8450a3346d4
Rsantct <rsantct@gmail.com>
2023-11-18 19:25:28 +0100

    Update paudio.sh

--- bin/paudio.sh
+++ bin/paudio.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 
-# Stopping audio
-pkill -f "server.py paudio"
-pkill -f "camilladsp.yml"
-
-# Stopping control web page
-pkill -f "paudio/code/share/www/nodejs"
-
-
 if [[ $1 = *"stop"* ]]; then
+
+    pkill -f "paudio/code/"
     echo "stopped."
     exit 0
 
 
-# Starting audio and control web page
 elif [[ $1 = *"start"* ]]; then
 
     # Control web page
     node ~/paudio/code/share/www/nodejs_www_server/www-server.js >/dev/null 2>&1 &
     echo "pAudio web server running in background ..."
 
-    # Verbose mode
+    # Audio in verbose mode
     if [[ $2 = *"-v"* ]]; then
-
         python3 ~/paudio/code/share/server.py paudio 0.0.0.0 9990 -v
         pkill -f "paudio/code/share/www/nodejs"
 
-    # Silent mode
+    # Audio in silent mode
     else
-
         python3 ~/paudio/code/share/server.py paudio 0.0.0.0 9990 1>/dev/null 2>&1 &
         echo "pAudio running in background ..."
 
