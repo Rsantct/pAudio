e94242355a0e413586708f3799c70729964e1e9f
Rsantct <rsantct@gmail.com>
2023-12-01 00:08:08 +0100

    Update paudio.sh

--- paudio/paudio.sh
+++ paudio/paudio.sh
@@ -8,17 +8,13 @@ function help {
 }
 
 
-function stop_all {
+function stop {
     pkill -f "paudio\/code\/"
     echo "pAudio has been stopped."
 }
 
 
-if [[ $1 = *"stop"* ]]; then
-
-    # Stop all (audio and web page)
-    stop_all
-
+function restore {
     echo "Restoring previous Default Playback Device"
     dev=$(cat ~/paudio/.previous_default_device)
     SwitchAudioSource -s "$dev"
@@ -26,21 +22,17 @@ if [[ $1 = *"stop"* ]]; then
     echo "Restoring previous Playback Device Volume"
     vol=$(cat ~/paudio/.previous_default_device_volume)
     osascript -e 'set volume output volume '$vol
+}
 
-    exit 0
 
-
-elif [[ $1 = *"start"* ]]; then
-
-    # Stop all (audio and web page)
-    stop_all
+function start {
 
     # Control web page
     node ~/paudio/code/share/www/nodejs_www_server/www-server.js >/dev/null 2>&1 &
     echo "pAudio web server running in background ..."
 
     # Audio in verbose mode
-    if [[ $2 = *"-v"* ]]; then
+    if [[ $1 = *"-v"* ]]; then
         python3 ~/paudio/code/share/server.py paudio 0.0.0.0 9990 -v
         pkill -f "paudio/code/share/www/nodejs"
 
@@ -49,6 +41,30 @@ elif [[ $1 = *"start"* ]]; then
         python3 ~/paudio/code/share/server.py paudio 0.0.0.0 9990 1>/dev/null 2>&1 &
         echo "pAudio running in background ..."
 
+    fi
+}
+
+
+if [[ $1 = *"stop"* ]]; then
+
+    stop
+    restore
+
+
+elif [[ $1 = *"start"* ]]; then
+
+    stop
+    start $2
+
+elif [[ $1 = *"toggle"* ]]; then
+
+    running=$(pgrep -f "paudio\/code\/")
+
+    if [[ $running ]]; then
+        stop
+        restore
+    else
+        start
     fi
 
 
