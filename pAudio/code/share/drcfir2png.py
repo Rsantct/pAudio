4179de72a058bbae677b48b5868fffc10072bc51
Rsantct <rsantct@gmail.com>
2025-08-08 21:44:51 +0200

    Update drcfir2png.py

--- pAudio/code/share/drcfir2png.py
+++ pAudio/code/share/drcfir2png.py
@@ -211,7 +211,7 @@ if __name__ == '__main__':
         print( f'(drcfir2png) using sample rate: {FS}' )
 
     # Get DRC sets names
-    drc_sets = get_drc_sets_from_loudspeaker_folder()
+    drc_sets = get_drc_fir_sets_from_loudspeaker_folder()
 
     # Do plot png files from pcm files
     drc_sets.append('none')
