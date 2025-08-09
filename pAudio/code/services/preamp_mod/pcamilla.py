a604046e3d7f04044687878e6f0636cba42f220e
Rsantct <rsantct@gmail.com>
2025-08-09 12:25:16 +0200

    Update pcamilla.py

--- pAudio/code/services/preamp_mod/pcamilla.py
+++ pAudio/code/services/preamp_mod/pcamilla.py
@@ -14,15 +14,21 @@ from    camilladsp      import  CamillaClient
 
 import  make_eq         as      mkeq
 
-from    pcamilla_mod.do_makes       import  *
-from    pcamilla_mod.do_clears      import  *
-
-
 UHOME       = os.path.expanduser('~')
 MAINFOLDER  = f'{UHOME}/pAudio'
 sys.path.append(f'{MAINFOLDER}/code/share')
 
 from    common import *
+
+from    pcamilla_mod.do_makes       import  *
+from    pcamilla_mod.do_clears      import  *
+import  pcamilla_mod.lspk_iir       as lspk_iir
+import  pcamilla_mod.base_config    as base_config
+
+lspk_iir.Fmt         =  Fmt
+base_config.Fmt      =  Fmt
+base_config.EQFOLDER =  EQFOLDER
+
 
 if sys.platform == 'linux' and CONFIG.get('jack'):
     import  jack
@@ -100,150 +106,6 @@ def _prepare_cam_config(pAudio_config):
         2. Translates pAudio configuration to the CamillaDSP config
     """
 
-    def prepare_base_config():
-
-        def prepare_devices():
-
-            chunksize = 1024
-
-            # Coreaudio
-            if pAudio_config.get('coreaudio'):
-
-                cam_config["devices"] = pAudio_config["coreaudio"].get('devices')
-
-                cam_config["devices"]["capture"] ["type"] = 'CoreAudio'
-                cam_config["devices"]["playback"]["type"] = 'CoreAudio'
-
-
-            # Jack
-            elif pAudio_config.get('jack'):
-
-                out_channels = 2
-
-                if pAudio_config.get('outputs'):
-                    out_channels = len( pAudio_config.get('outputs') )
-
-                if pAudio_config["jack"].get('period'):
-                    chunksize = pAudio_config["jack"].get('period')
-
-                cam_config["devices"] = {
-
-                    'capture': {    'channels':     2,
-                                    'device':       'default',
-                                    'type':         'Jack'
-                                },
-
-                    'playback': {   'channels':     out_channels,
-                                    'device':       'default',
-                                    'type':         'Jack'
-                                }
-                }
-
-            else:
-                print(f'{Fmt.BOLD}Audio backend still not supported{Fmt.END}')
-                sys.exit()
-
-
-            cam_config["devices"]["samplerate"]         = pAudio_config["samplerate"]
-
-            cam_config["devices"]["chunksize"]          = chunksize
-
-            cam_config["devices"]["silence_threshold"]  = -80
-
-            cam_config["devices"]["silence_timeout"]    = 30
-
-
-        def prepare_filters():
-
-            cam_config["filters"] =    {
-
-            # Balance and Polarity
-            'bal_pol_L':    {  'type': 'Gain',
-                                'parameters': {
-                                    'gain':     0.0,
-                                    'inverted': False,
-                                    'mute':     False
-                                }
-                            },
-            'bal_pol_R':    {  'type': 'Gain',
-                                'parameters': {
-                                    'gain':     0.0,
-                                    'inverted': False,
-                                    'mute':     False
-                                }
-                            },
-
-            # Dither
-            'dither':   {   'type': 'Dither',
-                            'parameters': {'bits': 16, 'type': 'Shibata441'},
-                        },
-
-            # DRC gain
-            'drc_gain': {   'type': 'Gain',
-                            'parameters': {
-                                    'gain':     0.0,
-                                    'inverted': False,
-                                    'mute':     False
-                            }
-                        },
-
-            # LU OFFSET (compensation for Loudness War)
-            'lu_offset': {  'type': 'Gain',
-                            'parameters': {
-                                    'gain':      0.0,
-                                    'inverted': False,
-                                    'mute':     False
-                            }
-                        },
-
-            # Preamp EQ (tones anf loudnes curves)
-            'preamp_eq':    {   'type': 'Conv',
-                                'parameters': {
-                                    'filename': f'{EQFOLDER}/eq_flat.pcm',
-                                    'format': 'FLOAT32LE',
-                                    'type': 'Raw'
-                                }
-                        }
-            }
-
-
-        def prepare_mixers():
-            """ Only preamp mixer at init
-            """
-
-            cam_config["mixers"] = {}
-
-            cam_config["mixers"]["preamp_mixer"] = make_mixer_preamp()
-
-
-        def prepare_pipeline():
-
-            cam_config["pipeline"] = [
-
-                # Input stereo preamp mixer
-                {   'type': 'Mixer', 'name': 'preamp_mixer'
-                },
-
-                # Stereo filtering at preamp stage
-                {   'description':  'preamp.L',
-                    'channels':     [0],
-                    'type':         'Filter',
-                    'names':        ['preamp_eq', 'drc_gain', 'lu_offset', 'bal_pol_L']
-                },
-                {   'description':  'preamp.R',
-                    'channels':     [1],
-                    'type':         'Filter',
-                    'names':        ['preamp_eq', 'drc_gain', 'lu_offset', 'bal_pol_R']
-                }
-            ]
-
-
-        prepare_devices()
-        prepare_filters()
-        prepare_mixers()
-        prepare_pipeline()
-
-
     def prepare_multiway_structure():
         """ The multiway N channel expander Mixer
         """
@@ -301,217 +163,19 @@ def _prepare_cam_config(pAudio_config):
         do_xo_stuff()
 
 
-    def update_drc_fir():
-
-        # drc filters
-        for drcset in pAudio_config["drc_sets"]:
-
-            for ch in 'L', 'R':
-
-                cam_config["filters"][f'drc.{ch}.{drcset}'] = make_drc_filter(  ch,
-                                                                                drcset,
-                                                                                pAudio_config["samplerate"],
-                                                                                LSPKFOLDER
-                                                                              )
-
-        # The initial pipeline points to the FIRST drc_set
-        insert_drc_to_pipeline(cam_config, drcID=pAudio_config["drc_sets"][0])
-
-
-    def update_peq_stuff():
-
-        # Filters section
-        for ch in pAudio_config["PEQ"]:
-            for peq, pms in pAudio_config["PEQ"][ch].items():
-                cam_config["filters"][f'peak.{ch}.{peq}'] = \
-                    make_peq_filter(pms["freq"], pms["gain"], pms["q"])
-
-        # Pipeline
-        npL = 0
-        npR = 0
-        for p in [x for x in cam_config["filters"] if x.startswith('peak.')]:
-            if '.L' in p:
-                cam_config["pipeline"][1]["names"].append(p)
-                npL += 1
-            elif '.R' in p:
-                cam_config["pipeline"][2]["names"].append(p)
-                npR += 1
-
-        # Filling with dummies to balance number of peaking in L and R
-        if npL != npR:
-            cam_config["filters"][f'peak.dummy'] = \
-                make_peq_filter(freq=20, gain=0.0, qorbw=1.0)
-        if npL < npR:
-            for i in range(npR - npL):
-                cam_config["pipeline"][1]["names"].append('peak.dummy')
-        if npR < npL:
-            for i in range(npL - npR):
-                cam_config["pipeline"][2]["names"].append('peak.dummy')
-
-
-    def update_lspk_iir():
-
-        # Import the filters
-
-        if not cam_config.get('filters'):
-            cam_config["filters"] = {}
-
-        for fname, fparams in pAudio_config["iir_eq"].items():
-            cam_config["filters"][fname] = fparams
-
-        # Pipeline step for loudspeaker EQ filters (will be applied at both channels)
-        pipeline_eq_L_step = {
-            'type':         'Filter',
-            'description':  f'{ pAudio_config["loudspeaker"] } (EQ left)',
-            'channels':     [0],
-            'bypassed':     False,
-            'names':        []
-        }
-        pipeline_eq_R_step = {
-            'type':         'Filter',
-            'description':  f'{ pAudio_config["loudspeaker"] } (EQ right)',
-            'channels':     [1],
-            'bypassed':     False,
-            'names':        []
-        }
-        pipeline_eq_L_step_names = []
-        pipeline_eq_R_step_names = []
-
-
-        # Pipeline step for loudspeaker DRC filters
-        pipeline_drc_L_step = {
-            'type':         'Filter',
-            'description':  f'{ pAudio_config["loudspeaker"] } (DRC left)',
-            'channels':     [0],
-            'bypassed':     False,
-            'names':        []
-        }
-        pipeline_drc_R_step = {
-            'type':         'Filter',
-            'description':  f'{ pAudio_config["loudspeaker"]} (DRC right)',
-            'channels':     [1],
-            'bypassed':     False,
-            'names':        []
-        }
-        pipeline_drc_L_step_names = []
-        pipeline_drc_R_step_names = []
-
-        # Iterate over loudspeaker filters
-        for f in pAudio_config["iir_eq"]:
-
-            # Filter is common for both channels
-            if not '_L_' in f and not '_R_' in f:
-
-                pipeline_eq_L_step_names.append(f)
-                print(f'{Fmt.BLUE}Adding filter `{f}` to pipeline `{pipeline_eq_L_step["description"]}`{Fmt.END}')
-                pipeline_eq_R_step_names.append(f)
-                print(f'{Fmt.BLUE}Adding filter `{f}` to pipeline `{pipeline_eq_R_step["description"]}`{Fmt.END}')
-
-            # Filter is for an specific channel (i.e. DRC)
-            else:
-
-                if '_L_' in f:
-                    pipeline_drc_L_step_names.append(f)
-                    print(f'{Fmt.BLUE}Adding filter `{f}` to pipeline `{pipeline_drc_L_step["description"]}`{Fmt.END}')
-
-                if '_R_' in f:
-                    pipeline_drc_R_step_names.append(f)
-                    print(f'{Fmt.BLUE}Adding filter `{f}` to pipeline `{pipeline_drc_R_step["description"]}`{Fmt.END}')
-
-        pipeline_eq_L_step["names"] = pipeline_eq_L_step_names
-        pipeline_eq_R_step["names"] = pipeline_eq_R_step_names
-
-        pipeline_drc_L_step["names"] = pipeline_drc_L_step_names
-        pipeline_drc_R_step["names"] = pipeline_drc_R_step_names
-
-        if pipeline_eq_L_step["names"] :
-            cam_config["pipeline"].append( pipeline_eq_L_step )
-            cam_config["pipeline"].append( pipeline_eq_R_step )
-
-        if pipeline_drc_L_step["names"] :
-            cam_config["pipeline"].append( pipeline_drc_L_step )
-            cam_config["pipeline"].append( pipeline_drc_R_step )
-
-
-    def update_dither():
-        """ Adjust the dither filter as per the output sample format and samplerate
-        """
-
-        if not( pAudio_config.get("coreaudio") and pAudio_config["coreaudio"]["devices"]["playback"].get("dither") ):
-            return
-
-        # First we need to remove dither parameter.
-        # It was included in pAudio playback device because logical order,
-        # but it is not a CamillaDSP devices parameter.
-        del cam_config["devices"]["playback"]["dither"]
-
-        # Update `dither` filter parameters
-
-        dither_bits = get_bit_depth( cam_config["devices"]["playback"]["format"] )
-
-        # https://github.com/HEnquist/camilladsp#dither
-        match cam_config["devices"]["samplerate"]:
-            case 44100:     d_type = 'Shibata441'
-            case 48000:     d_type = 'Shibata48'
-            case _:         d_type = 'Simple'
-
-        cam_config["filters"]["dither"] = make_dither_filter(d_type, dither_bits)
-
-        # Add dither to the last steps of the pipeline
-
-        step_type = ''
-        last_step_type = ''
-
-        for step in cam_config["pipeline"][::-1]:
-
-            if step.get('description'):
-
-                # Multiway have XOVER in last steps of the pipeline
-                if 'xover.' in step.get('description').lower():
-                    step_type = 'xover'
-
-                # Full Range can have PREAMP, and optionally EQ and/or DRC
-                else:
-
-                    if   '(drc'    in step.get('description').lower():
-                        step_type = 'drc'
-                    elif '(eq'     in step.get('description').lower():
-                        step_type = 'eq'
-                    elif 'preamp.' in step.get('description').lower():
-                        step_type = 'preamp'
-
-                if last_step_type and step_type != last_step_type:
-                    break
-
-                if step_type in ('xover', 'drc', 'eq', 'preamp'):
-                    step["names"].append('dither')
-
-                last_step_type = step_type
-
-
     # pAudio config DEBUG
     #print('--- pAudio ----')
     #print( yaml.dump(pAudio_config, default_flow_style=False, sort_keys=False, indent=2) )
 
-
     # From here `cam_config` will grow progressively
     cam_config = {}
 
-    # Prepare CamillaDSP base config
-    prepare_base_config()
+    # CamillaDSP base config
+    base_config.prepare_base_config(pAudio_config, cam_config)
 
-    # The PEQ **PENDING TO REVIEW**
-    # this intended to read a human readable user section
-    # update_peq_stuff()
-
-    # FIR DRCs **PENDING TO REVIEW**
-    #if pAudio_config["drc_sets"]:
-    #    update_drc_fir()
-
-    # IIR EQ filters: pAudio_config can have a set of CamillaDSP filters
-    #                 from the loudspeaker yaml file.
+    # IIR EQ filters previously imported from the loudspeaker folder 'camilla_dsp.yml' file
     if pAudio_config.get('iir_eq'):
-        update_lspk_iir()
+        lspk_iir.update_lspk_iir(pAudio_config, cam_config)
 
     # Multiway if more than 2 outputs
     outputs_in_use = [ x for x in pAudio_config["outputs"] if pAudio_config["outputs"][x].get('name') ]
@@ -519,7 +183,7 @@ def _prepare_cam_config(pAudio_config):
         prepare_multiway_structure()
 
     # Dither
-    update_dither()
+    base_config.update_dither(pAudio_config, cam_config)
 
     return cam_config
 
@@ -657,22 +321,6 @@ def init_camilladsp(pAudio_config):
 
         print(f'{Fmt.BOLD}ERROR loading CamillaDSP configuration. {str(e)}{Fmt.END}')
         return str(e)
-
-
-def insert_drc_to_pipeline(cfg, drcID = ''):
-    for n in (1,2):
-        names_old = cfg["pipeline"][n]['names']
-        names_new = list_remove_by_pattern(names_old, 'drc.')
-        names_new.insert(1, f'drc.L.{drcID}')
-        cfg["pipeline"][n]['names'] = names_new
-
-
-def append_item_to_pipeline(cfg, item = ''):
-    for n in (1,2):
-        names_old = cfg["pipeline"][n]['names']
-        names_new = list_remove_by_pattern(names_old, item)
-        names_new.append(item)
-        cfg["pipeline"][n]['names'] = names_new
 
 
 def reload_eq():
@@ -940,27 +588,7 @@ def set_xo(xo_set):
 
 def set_drc(drcID):
 
-    result = ''
-
-    cfg = CC.config.active()
-
-    if drcID == 'none':
-        try:
-            clear_pipeline_input_filters(cfg, pattern='drc.')
-            set_config_sync(cfg)
-            result = 'done'
-
-        except Exception as e:
-            result = f'(pcamilla.set_drc: `none`) ERROR: {str(e)}'
-
-    else:
-        try:
-            insert_drc_to_pipeline(cfg, drcID)
-            set_config_sync(cfg)
-            result = 'done'
-
-        except Exception as e:
-            result = f'(pcamilla.set_drc: `{drcID}`) ERROR: {str(e)}'
+    result = 'WIP'
 
     return result
 
