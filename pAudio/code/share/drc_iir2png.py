7cc3c04349d0d4e0417c6641743ec4d92ba4ee9e
Rsantct <rsantct@gmail.com>
2025-08-09 22:59:03 +0200

    Create drc_iir2png.py

---
+++ pAudio/code/share/drc_iir2png.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Rafael Sánchez
+# This file is part of 'pAudio', a PC based personal audio system.
+
+""" This program makes plot from a set of parametric equalizers
+"""
+
+if __name__ == "__main__":
+
+    print(f'(drc_iir2png) WIP')
+
