e405097691eb8399f74ee915251e55973107cb4e
Rsantct <rsantct@gmail.com>
2025-08-23 19:47:52 +0200

    Update paudio_update.sh

--- bin/paudio_update.sh
+++ bin/paudio_update.sh
@@ -43,7 +43,7 @@ cp ~/pAudio/config.yml.BAK ~/pAudio/config.yml 1>/dev/null 2>&1
 
 # Stop any paudio backend
 pkill -f "server.py paudio "        1>/dev/null 2>&1
-killall -KILL "camilladsp"          1>/dev/null 2>&1
+pkill -f "camilladsp"               1>/dev/null 2>&1
 
 echo
 
