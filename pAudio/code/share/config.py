33c891dcf76eb6573d507692f3d9c26fe24178c3
Rsantct <rsantct@gmail.com>
2026-01-02 15:53:42 +0100

    Update config.py

--- pAudio/code/share/config.py
+++ pAudio/code/share/config.py
@@ -387,6 +387,9 @@ def _init():
     if not 'ref_level_gain_offset' in CONFIG:
         CONFIG["ref_level_gain_offset"] = 0.0
 
+    if not 'loudness_compensation_clamped_above_zero' in CONFIG:
+        CONFIG["loudness_compensation_above_zero"] = False
+
     if not "tones_span_dB" in CONFIG:
         CONFIG["tones_span_dB"] = 6.0
 
