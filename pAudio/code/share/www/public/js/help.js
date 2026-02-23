d5423fc48672bd5b154a11b900ae332b2536c9e6
Rsantct <rsantct@gmail.com>
2026-02-24 00:40:31 +0100

    Create help.js

---
+++ pAudio/code/share/www/public/js/help.js
@@ -0,0 +1,72 @@
+/*
+    Copyright (c) Rafael Sánchez
+    This file is part of 'pAudio', a PC based personal audio system.
+*/
+
+export const HELP_EN = `
+The volume MUST be controlled here.
+
+[LC] Loudness Contour compensation allows a tonal balanced low volume listenting experience.
+
+[0.0] dB means the normal "loud SPL" for your listening position, say around 75~80 dBSPL.
+
+CHECK that your DAC and AMPLIFIER are running at "full volume" to reach your target "loud SPL".
+
+If your music program is loud (most CDs are), use the LU_offset slider to compensate.
+
+LU_monitor indicates approximately how much excess volume your music program has.
+
+Some LU_offset settings:
+
+- 0 dB: very rare recordings. The BIS Records label is a good reference.
+- 6 dB: a good mastered CD
+- 9 dB: most CD even in classical
+- 12 dB: most pop music CD
+- 15 dB: ultra compressed music, usually in pop
+`
+
+export const HELP_CAT = `
+El volum HA DE ser controlat aquí.
+
+[LC] 'Loudness Contour'. La compensació de contorn de sonoritat permet una experiència d'escolta de baix volum amb un equilibri tonal.
+
+[0.0] dB significa el "SPL fort" normal per a la vostra posició d'escolta, per exemple, al voltant de 75~80 dBSPL.
+
+COMPROVEU que el vostre DAC i AMPLIFICADOR funcionin a "volum complet" per assolir el vostre "SPL fort" objectiu.
+
+Si el vostre programa de música és alt (la majoria de CD ho són), utilitzeu el control lliscant LU_offset per compensar.
+
+LU_monitor indica aproximadament quant volum d'excés té el vostre programa de música.
+
+Alguns paràmetres de LU_offset:
+
+- 0 dB: enregistraments molt poc freqüents. El segell BIS Records és una bona referència.
+- 6 dB: un bon CD masteritzat
+- 9 dB: la majoria de CD, fins i tot en música clàssica
+- 12 dB: la majoria de CD de música pop
+- 15 dB: música ultracomprimida, normalment en música pop
+`
+
+export const HELP_SP = `
+El volumen DEBE controlarse aquí.
+
+La compensación del contorno de sonoridad
+
+[LC] 'Loudness Contour' permite una experiencia auditiva a bajo volumen con equilibrio tonal.
+
+[0.0] dB significa el "SPL alto" normal para su posición de escucha, aproximadamente entre 75 y 80 dB SPL.
+
+COMPRUEBE que su DAC y amplificador funcionen a "máximo volumen" para alcanzar el "SPL alto" objetivo.
+
+Si su programa de música tiene un volumen alto (la mayoría de los CD lo tienen), utilice el control deslizante LU_offset para compensar.
+
+LU_monitor indica aproximadamente el exceso de volumen de su programa de música.
+
+Algunos ajustes de LU_offset:
+
+- 0 dB: grabaciones muy poco frecuentes. El sello BIS Records es una buena referencia.
+- 6 dB: un CD masterizado de calidad
+- 9 dB: la mayoría de los CD, incluso de música clásica
+- 12 dB: la mayoría de los CD de música pop
+- 15 dB: música ultracomprimida, generalmente de música pop
+`
