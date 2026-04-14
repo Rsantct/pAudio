0814bd1f233bff11eb56d16f6e1e9b7dafd72a7a
Rsantct <rsantct@gmail.com>
2026-04-14 17:48:18 +0200

    Update common.py

--- pAudio/code/share/common.py
+++ pAudio/code/share/common.py
@@ -59,6 +59,28 @@ def loop_file_changed(filepath, what_to_do):
     observer.start()
 
 
+def dict_compare(d1, d2, static=True):
+    """ Compare dictionaries
+        static: boolean to find only static keys changes, it is FASTER
+    """
+
+    if static:
+        changes = {k: (d1[k], d2[k]) for k in d1 if k in d2 and d1[k] != d2[k]}
+        return changes
+
+
+    keys1 = set(d1.keys())
+    keys2 = set(d2.keys())
+
+    interseccion = keys1.intersection(keys2)
+
+    changed = {k: (d1[k], d2[k]) for k in interseccion if d1[k] != d2[k]}
+    added   = {k: d2[k] for k in keys2 - keys1}
+    removed = {k: d1[k] for k in keys1 - keys2}
+
+    return changed, added, removed
+
+
 def json_string_fix(cad):
     """ Example of a raw string that cannot be parsed by json.loads because nested double quotes (")
 
