9634f15d5825c9acce985a8987890d9c20d6b52f
Rsantct <rsantct@gmail.com>
2026-06-13 22:59:03 +0200

    Add function to get local IP address with socket
    Added a new function to retrieve the local IP address using a socket.

--- pAudio/code/share/common.py
+++ pAudio/code/share/common.py
@@ -1051,9 +1051,27 @@ def is_IP(s):
          return False
 
 
+def get_my_ip_with_socket():
+    """ retrieves the own IP address using socket
+        (string)
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # No se necesita conexión real, el IP puede ser cualquiera
+        s.connect(('8.8.8.8', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
 def get_my_ip():
     """ retrieves the own IP address
         (string)
+
+        We prefer this instead of using a dummy socket
     """
     try:
         tmp = sp.check_output( 'hostname --all-ip-addresses'.split() ).decode()
