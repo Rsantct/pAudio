c6539b2ff9ec184c6667aef61bb063b7097a4544
Rsantct <rsantct@gmail.com>
2026-06-13 22:37:17 +0200

    Add script to measure delay with jack_delay
    This script runs 'jack_delay' for a specified duration and logs output.

---
+++ bin/paudio_slave_tools/net_lspk_meas_delay_start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+MINUTES=5
+duration_sec=$(($MINUTES * 60))
+
+LOG_PATH="/tmp/lspk_delay_info.log"
+ERR_PATH="/tmp/lspk_delay_info.err"
+
+killall jack_delay
+
+if [[ $1 == *"-q"* ]]; then
+    echo "jack_delay stopped."
+    exit 0
+fi
+
+jack_delay -O right_lspk_send:in_3 -I right_lspk_recv:out_1 1>"$LOG_PATH" 2>"$ERR_PATH" &
+
+echo "running jack_delay for "$MINUTES" minutes"
+
+(sleep $duration_sec && \
+killall jack_delay && \
+echo "jack_delay stopped after "$MINUTES" minutes") &
