d0efe50120e73c5391ac4ab160d91b2c3943d142
Rsantct <rsantct@gmail.com>
2026-06-14 22:07:55 +0200

    Update wifi_audio_hot_spot.sh for better configuration

--- bin/paudio_slave_tools/networks/master/wifi_audio_hot_spot.sh
+++ bin/paudio_slave_tools/networks/master/wifi_audio_hot_spot.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-sudo nmcli connection delete "Enlace-Audio-Wifi" &>/dev/null
+# Choose the better channel on your zone
+CH=13
+
+# Configuración de contraseña (mínimo 8 caracteres)
+PASSWORD="xxxxxxxxxxxxxx"
+
+CON_NAME="Enlace-Audio-Wifi"
+
+sudo nmcli connection delete "$CON_NAME" &>/dev/null
 
 if [[ $1 == *'-q'* ]]; then
-    echo "Enlace-Audio-Wifi has been removed"
+    echo "$CON_NAME"" has been removed"
     exit 0
 fi
 
-# Creamos el AP totalmente ABIERTO (sin claves) en 2.4 GHz
-sudo nmcli connection add type wifi ifname wlan1 con-name "Enlace-Audio-Wifi" ssid "AudioPrivateNet" \
+# ancho 20 MHz más inmunidad radioeléctrica
+sudo nmcli connection add type wifi ifname wlan1 con-name "$CON_NAME" ssid "zitawifi" \
     802-11-wireless.mode ap \
     802-11-wireless.band bg \
-    802-11-wireless.channel 1 \
+    802-11-wireless.channel $CH \
+    802-11-wireless.channel-width 20 \
     ipv4.addresses 192.168.20.1/24 \
     ipv4.method manual \
-    ipv4.never-default yes
+    ipv4.never-default yes \
+    802-11-wireless-security.key-mgmt wpa-psk \
+    802-11-wireless-security.psk "$PASSWORD" \
+    802-11-wireless-security.proto rsn \
+    802-11-wireless-security.pairwise ccmp \
+    802-11-wireless-security.group ccmp
 
-sudo nmcli connection up "Enlace-Audio-Wifi"
 
+sudo nmcli connection up "$CON_NAME"
