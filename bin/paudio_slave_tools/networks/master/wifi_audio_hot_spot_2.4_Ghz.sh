7696bec54eeaa4b857d18a5971a2c7fefab7c122
Rsantct <rsantct@gmail.com>
2026-06-15 13:10:58 +0200

    Add powersave option to WiFi connection setup

--- bin/paudio_slave_tools/networks/master/wifi_audio_hot_spot_2.4_Ghz.sh
+++ bin/paudio_slave_tools/networks/master/wifi_audio_hot_spot_2.4_Ghz.sh
@@ -15,12 +15,13 @@ if [[ $1 == *'-q'* ]]; then
     exit 0
 fi
 
-# ancho 20 MHz más inmunidad radioeléctrica
+# ancho 20 MHz más inmunidad radioeléctrica, powersave 2 (OFF)
 sudo nmcli connection add type wifi ifname wlan1 con-name "$CON_NAME" ssid "zitawifi" \
     802-11-wireless.mode ap \
     802-11-wireless.band bg \
     802-11-wireless.channel $CH \
     802-11-wireless.channel-width 20 \
+    802-11-wireless.powersave 2 \
     ipv4.addresses 192.168.20.1/24 \
     ipv4.method manual \
     ipv4.never-default yes \
